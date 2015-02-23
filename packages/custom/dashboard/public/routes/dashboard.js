6c6272bf2859d3b5e37e4ce81f0d04cfdfd60d8a
papesdiop <papesdiop@gmail.com>
2015-02-23 17:22:12 +0000

    mobile first dashboard features impl

--- packages/custom/dashboard/public/routes/dashboard.js
+++ packages/custom/dashboard/public/routes/dashboard.js
@@ -9,6 +9,14 @@ angular.module('mean.dashboard').config(['$stateProvider',
         .state('dashboard.appointments', {
             url: '/appointments',
             templateUrl: 'dashboard/views/appointments.html'
+        })
+        .state('dashboard.notifications', {
+            url: '/notifications',
+            templateUrl: 'dashboard/views/notifications.html'
+        })
+        .state('dashboard.calendar', {
+            url: '/calendar',
+            templateUrl: 'dashboard/views/calendar.html'
         });
   }
 ]);
