c62b8f8ae8ce2e7d6851bd6000006cee90ed6bc2
haythem <haythem.horbit@gmail.com>
2015-08-18 20:42:12 +0100

    bug fixes

--- web/config.js
+++ web/config.js
@@ -4,5 +4,5 @@
  */
 serverApi="http://localhost:3000/api"
 //serverApi="http://169.54.30.34:3000/api"
-//serverApi="http://tracdr.c4asolution.com:3000/api"
+serverApi="http://tracdr.c4asolution.com:3000/api"
 APP.value('ApiServer', serverApi)
